2261f0a75124916f6b64f28a85322a465eccd68a
0BAB1 <62248962+0BAB1@users.noreply.github.com>
2023-10-12 21:35:39 +0200

    exo1

--- Ex2a.m
+++ Ex2a.m
@@ -1,9 +1,13 @@
+%exactement comme l'ex1 sauf que l'on fait directement la fonction de
+%transfert pour zeta = 0.1 et en utilisant la methode tf intégrée a matlab
 clf; clear all; close all;
 m=1
 zeta=0.1
 k=1
 wn=sqrt(k/m)
-den=[...] %TODO
-num=[...] %TODO
+%Travail a faire:
+num=[1/m]
+den=[1 2*zeta*wn wn^2]
+%fin tarvail a faire
 sys=tf(num,den)
-bode(sys)
+bode(sys) % on utilise ensuite la méthode "bode" pour tracer !
